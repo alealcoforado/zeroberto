e01501492c1cabc0e8748cdf9f7243ef19867b88
Alexandre Alcoforado <alealcoforado@gmail.com>
2023-02-11 04:19:28 -0300

    teste

--- myscript.sh
+++ myscript.sh
@@ -1,26 +1,36 @@
 # This is my first bash script
 # ./myscript.sh "patch/minor/major" "commit_message"
 
+tag=$( tail -n 1 versions.txt )
 
-# echo "Hello"
-# echo $1 $2
+echo "current_version:" $tag
 
-# lastversion = $( tail -n 1 versions.txt )
-
-# tac versions.txt |egrep -m 1 .
-
-
-# ## get last line from versions.txt
-# $last_version awk '/./{line=$0} END{print line}' versions.txt
-
-
-git add .  
-read -p "Commit description: " desc  
+read -p "Commit comment: " desc  
 read -p "patch/major/minor: " commit_type  
 
+arrIN=(${tag//./ })
 
+major_version=${arrIN[0]}
+minor_version=${arrIN[1]}
+patch_version=${arrIN[2]}
+
+case $1 in 
+"major") let "major_version=major_version+1" "minor_version=0" "patch_version=0";;
+"minor") let "minor_version=minor_version+1" "patch_version=0";;
+"patch") let "patch_version=patch_version+1";;
+*) echo "Opção inválida. Opções: major/minor/patch"; exit 0 ;;
+esac
+
+# echo "new_version: "$major_version.$minor_version.$patch_version
+new_version=$major_version.$minor_version.$patch_version
+echo "new_version: "$new_version
+
+git add .  
 git commit -m "$desc"
 git push origin main
+echo $new_version >>versions.txt
+echo "Commited to github"
 
-foo=1:2:3:4:5
-$ echo ${foo##*:}
+
+
+
