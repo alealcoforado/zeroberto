a514b87bdbbd6f79630c968d55863a45c711726a
Alexandre Alcoforado <alealcoforado@gmail.com>
2023-05-17 00:21:47 -0300

    fixed dataset size

--- train_eval.py
+++ train_eval.py
@@ -83,7 +83,7 @@ def main():
 
     # Open the dataset
     dataset = load_dataset(args.dataset)
-    train_dataset = dataset[args.dataset_train_split].select(range(0,min(len(dataset[args.dataset_train_split]), 100)))
+    train_dataset = dataset[args.dataset_train_split].select(range(0,min(len(dataset[args.dataset_train_split]), 5000)))
     # args.dataset_test_split = "test" # TO DO remove
     test_dataset = dataset[args.dataset_test_split]#.select(range(0,200))
 
